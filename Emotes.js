f5e8db0a38613281cf0ac9d54dfd329911379c9e
Victor Dyotte <Vdyotte@gmail.com>
2014-05-01 19:57:32 -0400

    Create Emotes.js

---
+++ Emotes.js
@@ -0,0 +1,7 @@
+
+var kappa = {
+    name: Kappa,
+    regex: /Kappa/g,
+    image: "<img src="http://static-cdn.jtvnw.net/jtv_user_pictures/' +
+                    'chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png">"
+}
