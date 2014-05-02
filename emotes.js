17d0f3b4804ba53bf9c5f1760a8af185dd22fbfa
Victor Dyotte <Vdyotte@gmail.com>
2014-05-01 21:24:34 -0400

    Update and rename Emotes.js to emotes.js

--- emotes.js
+++ emotes.js
@@ -1,6 +1,6 @@
 var emotes = [
     {
-        name: Kappa,
+        name: 'Kappa',
         regex: /Kappa/g,
         image: '<img src="http://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png">'
     },
