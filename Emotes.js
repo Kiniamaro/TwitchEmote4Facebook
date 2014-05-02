8cee0dd1c0f05e78af1e0ecc0d2299096e6c30d8
Victor Dyotte <Vdyotte@gmail.com>
2014-05-01 20:04:04 -0400

    Update Emotes.js

--- Emotes.js
+++ Emotes.js
@@ -1,7 +1,13 @@
-
-var kappa = {
-    name: Kappa,
-    regex: /Kappa/g,
-    image: "<img src="http://static-cdn.jtvnw.net/jtv_user_pictures/' +
-                    'chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png">"
-}
+var emotes = [
+    {
+        name: Kappa,
+        regex: /Kappa/g,
+        image: "<img src="http://static-cdn.jtvnw.net/jtv_user_pictures/' +
+                        'chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png">"
+    },
+    
+    {
+        
+    }
+    
+]
