6b258b51974f83867c978a788f2ed93b7191d4e6
Alexandre Gagnon <alxgnon@gmail.com>
2014-05-01 23:58:02 -0400

    Misc improvements

--- TwitchEmotes4Facebook.user.js
+++ TwitchEmotes4Facebook.user.js
@@ -7,42 +7,36 @@
 // @grant         none
 // ==/UserScript==
 
+function twitchImg(file) {
+    return '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/' + file + '">';
+}
+
 var emotes = [
-    {name: 'Kappa', regex: /Kappa/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png">'},
-    {name: 'TriHard', regex: /TriHard/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-6407e6947eb69e21-24x30.png">'},
-    {name: 'FrankerZ', regex: /FrankerZ/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-3b96527b46b1c941-40x30.png">'},
-    {name: 'Kreygasm', regex: /Kreygasm/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-3a624954918104fe-19x27.png">'},
-    {name: 'FailFish', regex: /FailFish/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-c8a77ec0c49976d3-22x30.png">'},
-    {name: 'BibleThump', regex: /BibleThump/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-f6c13c7fc0a5c93d-36x30.png">'}
+    [/Kappa/g,      twitchImg('chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png')],
+    [/TriHard/g,    twitchImg('chansub-global-emoticon-6407e6947eb69e21-24x30.png')],
+    [/FrankerZ/g,   twitchImg('chansub-global-emoticon-3b96527b46b1c941-40x30.png')],
+    [/Kreygasm/g,   twitchImg('chansub-global-emoticon-3a624954918104fe-19x27.png')],
+    [/FailFish/g,   twitchImg('chansub-global-emoticon-c8a77ec0c49976d3-22x30.png')],
+    [/BibleThump/g, twitchImg('chansub-global-emoticon-f6c13c7fc0a5c93d-36x30.png')]
 ];
 
 var observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mut) {
-        var msglist = mut.target.getElementsByClassName('null');
-        for (var i = 0; i < msglist.length; i++) {
-            var msg = msglist[i];
-            textToEmote(msg);
+    for (var i = 0; i < mutations.length; mutations++) {
+        var msglist = mutations[i].target.getElementsByClassName('null');
+        for (var j = 0; j < msglist.length; j++) {
+            for (var k = 0; k < emotes.length; k++) {
+                if (msglist[j].textContent.match(emotes[k][0])) {
+                    msglist[j].innerHTML = msglist[j].innerHTML.replace(emotes[k][0], emotes[k][1]);
+                }
+            }
         }
-    });
+    };
 });
 
-function documentReady() {
+var readyChecker = setInterval(function () {
     var chatContainer = document.getElementById('ChatTabsPagelet');
-    observer.observe(chatContainer, {childList: true, subtree: true});
-}
-
-var readyStateChecker = setInterval(function () {
-    if (document.readyState === 'complete') {
-        documentReady();
-        clearInterval(readyStateChecker);
+    if (chatContainer != null) {
+        observer.observe(chatContainer, {childList: true, subtree: true});
+        clearInterval(readyChecker);
     }
 }, 100);
-
-function textToEmote(message) {
-    for(var i = 0; i < emotes.length; i ++) {
-        if (message.textContent.match(emotes[i].regex)) {
-            message.innerHTML = message.innerHTML.replace(emotes[i].regex, emotes[i].image);
-        }
-    }
-}
-
