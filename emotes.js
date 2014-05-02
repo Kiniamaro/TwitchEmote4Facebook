e6704791fc8eae8d20b7558327922f8e9d3b7555
kiniamaro <vdyotte@gmail.com>
2014-05-02 18:48:04 -0400

    Created branch for working on storing emotes externaly

---
+++ emotes.js
@@ -0,0 +1,39 @@
+
+var emotes = [
+
+    {
+        name: 'Kappa',
+        regex: /Kappa/g,
+        image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png">'
+    },
+    
+    {
+        name: 'TriHard',
+        regex: /TriHard/g,
+        image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-6407e6947eb69e21-24x30.png">'
+    },
+    
+    {
+        name: 'FrankerZ',
+        regex: /FrankerZ/g,
+        image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-3b96527b46b1c941-40x30.png">'
+    },
+     
+    {
+        name: 'Kreygasm',
+        regex: /Kreygasm/g,
+        image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-3a624954918104fe-19x27.png">'
+    },
+    
+    {
+        name: 'FailFish',
+        regex: /FailFish/g,
+        image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-c8a77ec0c49976d3-22x30.png">'
+    },
+    
+    {
+        name: 'BibleThump',
+        regex: /BibleThump/g,
+        image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-f6c13c7fc0a5c93d-36x30.png">'
+    }
+];
