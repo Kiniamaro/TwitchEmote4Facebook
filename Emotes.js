97facbbbdb6192ba5b28830bf82167bcaec881ab
Victor Dyotte <Vdyotte@gmail.com>
2014-05-01 21:22:08 -0400

    Update Emotes.js

--- Emotes.js
+++ Emotes.js
@@ -2,7 +2,7 @@ var emotes = [
     {
         name: Kappa,
         regex: /Kappa/g,
-        image: "<img src='http://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png'>"
+        image: '<img src="http://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png">'
     },
     
     {
