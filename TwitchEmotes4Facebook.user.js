9e57401b164d2a8cc1f3f71b8fc687906ddc41b3
kiniamaro <vdyotte@gmail.com>
2014-05-02 19:08:50 -0400

    Added a script element and attach it to the document head on load

--- TwitchEmotes4Facebook.user.js
+++ TwitchEmotes4Facebook.user.js
@@ -7,14 +7,11 @@
 // @grant         none
 // ==/UserScript==
 
-var emotes = [
-    {name: 'Kappa', regex: /Kappa/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png">'},
-    {name: 'TriHard', regex: /TriHard/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-6407e6947eb69e21-24x30.png">'},
-    {name: 'FrankerZ', regex: /FrankerZ/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-3b96527b46b1c941-40x30.png">'},
-    {name: 'Kreygasm', regex: /Kreygasm/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-3a624954918104fe-19x27.png">'},
-    {name: 'FailFish', regex: /FailFish/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-c8a77ec0c49976d3-22x30.png">'},
-    {name: 'BibleThump', regex: /BibleThump/g, image: '<img src="https://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-f6c13c7fc0a5c93d-36x30.png">'}
-];
+
+var emotePage = document.createElement('script');
+emotePage.setAttribute('src', 'https://raw.githubusercontent.com/Kiniamaro/Twitc' +
+                                 'hEmote4Facebook/EXTERNAL_EMOTES/emotes.js');
+document.getElementsByTagName('head')[0].appendChild(emotPage);
 
 var observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mut) {
@@ -45,4 +42,5 @@ function textToEmote(message) {
         }
     }
 }
+
 
