a00bb8eee615bed199727840b7345227b82ee905
Victor Dyotte <Vdyotte@gmail.com>
2014-05-01 21:52:15 -0400

    added more emotes!

--- emotes.js
+++ emotes.js
@@ -6,7 +6,33 @@ var emotes = [
     },
     
     {
-        
+        name: 'TriHard',
+        regex: /TriHard/g,
+        image '<img src="http://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-6407e6947eb69e21-24x30.png">'
+    },
+    
+    {
+        name: 'FrankerZ',
+        regex: /FrankerZ/g,
+        image '<img src="http://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-3b96527b46b1c941-40x30.png">'
+    },
+    
+    {
+        name: 'Kreygasm',
+        regex: /TriHard/g,
+        image '<img src="http://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-3a624954918104fe-19x27.png">'
+    },
+    
+    {
+        name: 'FailFish',
+        regex: /FailFish/g,
+        image '<img src="http://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-c8a77ec0c49976d3-22x30.png">'
+    },
+    
+    {
+        name: 'BibleThump',
+        regex: /BibleThump/g,
+        image '<img src="http://static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-f6c13c7fc0a5c93d-36x30.png">'
     }
     
-]
+];
